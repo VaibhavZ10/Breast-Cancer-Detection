e0b244699fbbcd39934adf4983cd844bd9995f8a
VaibhavZ10 <55946394+VaibhavZ10@users.noreply.github.com>
2021-11-24 04:25:29 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import pickle
 import numpy as np
 from flask import Flask, request, jsonify, render_template
-import GG
+import GG 
 from sklearn.preprocessing import StandardScaler
 
-app = Flask(__name__,static_url_path='/static')
+app = Flask(__name__, static_url_path='/static')
 model = pickle.load(open('model.pkl', 'rb'))
 scaler = pickle.load(open('scaler.pkl', 'rb'))
 
@@ -13,27 +13,27 @@ scaler = pickle.load(open('scaler.pkl', 'rb'))
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
 def predict():
 
     features = [float(x) for x in request.form.values()]
     final_features = [np.array(features)]
-    final_features = scaler.transform(final_features)    
+    final_features = scaler.transform(final_features)
     prediction = model.predict(final_features)
     y_probabilities_test = model.predict_proba(final_features)
     y_prob_success = y_probabilities_test[:, 1]
-    print("final features",final_features)
-    print("prediction:",prediction)
+    print("final features", final_features)
+    print("prediction:", prediction)
     output = round(prediction[0], 2)
-    y_prob=round(y_prob_success[0], 3)
+    y_prob = round(y_prob_success[0], 3)
     print(output)
 
     if output == 0:
         return render_template('index.html', prediction_text='THE PATIENT IS MORE LIKELY TO HAVE A BENIGN CANCER WITH PROBABILITY VALUE  {}'.format(y_prob))
     else:
-         return render_template('index.html', prediction_text='THE PATIENT IS MORE LIKELY TO HAVE A MALIGNANT CANCER WITH PROBABILITY VALUE  {}'.format(y_prob))
-        
-@app.route('/predict_api',methods=['POST'])
+        return render_template('index.html', prediction_text='THE PATIENT IS MORE LIKELY TO HAVE A MALIGNANT CANCER WITH PROBABILITY VALUE  {}'.format(y_prob))
+
+
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
 
     data = request.get_json(force=True)
@@ -41,6 +41,7 @@ def predict_api():
 
     output = prediction[0]
     return jsonify(output)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
